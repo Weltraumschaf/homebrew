8fde6f37c298a9126cc0bcf1f9ee9244e27848d0
Sven Strittmatter <ich@weltraumschaf.de>
2024-01-22 10:56:38 +0100

    Update to latest version
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- sshpass.rb
+++ sshpass.rb
@@ -2,12 +2,13 @@
 # Rubydoc: https://rubydoc.brew.sh/Formula.html
 class Sshpass < Formula
   desc <<~DESC
-    Sshpass is a tool for non-interactivly performing password authentication with SSH's so called "interactive keyboard password authentication".
-    Most user should use SSH's more secure public key authentiaction instead.
+    Sshpass is a tool for non-interactivly performing password authentication with SSH's so called 
+    "interactive keyboard password authentication". Most user should use SSH's more secure public 
+    key authentiaction instead.
   DESC
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz'
+  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.10/sshpass-1.10.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
-  sha256 'c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60'
+  sha256 'ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda'
 
   def install
     system "./configure",
